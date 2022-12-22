87172b7c7364c127cdaa262b3ae04022a0d0e673
jackvan20000312 <jackvan20000312@gmail.com>
2022-12-22 00:40:25 -0500

    adsf

--- HTMLCodeGenerator.py
+++ HTMLCodeGenerator.py
@@ -24,11 +24,14 @@ with open('professors.csv', 'r') as csv_file:
     department = row[3]
     research_interest = row[4]
     website = row[5]
-    hiring_link = row[6]
+    hiring_description = row[6]
+    hiring_website = row[7]
     
     # Determine whether the professor is hiring
-    if hiring_link:
-      hiring_html = f'<td valign="top"><a href="{hiring_link}" class="new-window"><b>Form</b></a></td>'
+    if hiring_description and hiring_website:
+      hiring_html = f'<td valign="top"><a href="{hiring_website}" class="new-window"><b>{hiring_description}</b></a></td>'
+    elif hiring_description:
+      hiring_html = f'<td valign="top"><a href="https://www.uscengr.com" class="new-window"><b>{hiring_description}</b></a></td>'
     else:
       hiring_html = '<td valign="top">No Positions</td>'
     
