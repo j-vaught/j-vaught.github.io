a0adb8a8af63be12562d8f47a72833aec8397a8a
jackvan20000312 <jackvan20000312@gmail.com>
2022-12-25 17:11:57 -0500

    shortened codebase

---
+++ public/searchTable.js
@@ -0,0 +1,23 @@
+function searchTable() {
+    // Declare variables
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("Table");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      for(j=0;j<6;j++){
+        td = tr[i].getElementsByTagName("td")[j];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+            j=6;
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+  }
